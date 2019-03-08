d83519d051b8cb9a3fdba8dc48a1c061a885cec4
David Tverdota <fireblade.sg@gmail.com>
2019-03-08 18:48:02 +0000

    Initial commit

---
+++ app/src/main/java/com/fireblade/londoneyegeo/GeofenceActivity.kt
@@ -0,0 +1,12 @@
+package com.fireblade.londoneyegeo
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class GeofenceActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_geofence)
+  }
+}
